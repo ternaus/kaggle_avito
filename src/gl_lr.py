c62875197b6be4d8aa8a6a37a77ad2591227fe1d
Vladimir Iglovikov <iglovikov@gmail.com>
2015-07-19 11:34:49 -0700

    There is something wrong with how do I calculate score with graphlab. Let's try standard sklearn

--- src/gl_lr.py
+++ src/gl_lr.py
@@ -9,12 +9,16 @@ I will try to use logistic regression from Graphlab to predict
 import graphlab as gl
 import os
 import scipy as sp
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 def llfun(act, pred):
     epsilon = 1e-15
     pred = sp.maximum(epsilon, pred)
     pred = sp.minimum(1 - epsilon, pred)
-    ll = sum(act * sp.log(pred) + sp.subtract(1,act) * sp.log(sp.subtract(1, pred)))
+    ll = sum(act * sp.log(pred) + sp.subtract(1, act) * sp.log(sp.subtract(1, pred)))
     ll = ll * -1.0/len(act)
     return ll
 
@@ -29,10 +33,7 @@ print 'filling missing values'
 a = train['Price'].mean()
 train = train.fillna('Price', a)
 
-print 'splitting set'
-sf_train, sf_test = train.random_split(0.5, seed=42)
-
-features=['Position',
+features = ['Position',
           'HistCTR',
           'Price',
           # 'CategoryID',
@@ -40,16 +41,39 @@ features=['Position',
           # 'LocationID'
           ]
 
-model = gl.logistic_classifier.create(sf_train,
-                                      target='IsClick',
-                                      features=features,
-                                      validation_set=sf_test)
+X = train[:10**6][features]
+y = train[:10**6].IsClick
 
-print 'predicting'
-prediction = model.predict(sf_test, output_type='probablility')
+random_state = 42
 
-print prediction[:30]
-print sf_test['IsClick'][:30]
-print 'calculating score'
-score = llfun(sf_test['IsClick'], prediction)
-print score
+# Learn
+clf = LogisticRegression(random_state=random_state)
+
+skf = cross_validation.StratifiedKFold(y, n_folds=5, shuffle=True, random_state=random_state)
+scores = cross_validation.cross_val_score(clf, X, y, cv=skf, scoring='log_loss', n_jobs=-1)
+print -np.mean(scores), np.std(scores)
+
+# print 'splitting set'
+# sf_train, sf_test = train.random_split(0.5, seed=42)
+#
+# features=['Position',
+#           'HistCTR',
+#           'Price',
+#           # 'CategoryID',
+#           # 'AdID',
+#           # 'LocationID'
+#           ]
+#
+# model = gl.logistic_classifier.create(sf_train,
+#                                       target='IsClick',
+#                                       features=features,
+#                                       validation_set=sf_test)
+#
+# print 'predicting'
+# prediction = model.predict(sf_test, output_type='probablility')
+#
+# print prediction[:30]
+# print sf_test['IsClick'][:30]
+# print 'calculating score'
+# score = llfun(sf_test['IsClick'], prediction)
+# print score
